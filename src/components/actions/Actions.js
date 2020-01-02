8d04bf6b35f0f5160d01492e7384259ba2cf93b7
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-01 21:30:03 -0800

    transfered over actions comp & css

---
+++ src/components/actions/Actions.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styles from './actions.css';
+import PropTypes from 'prop-types';
+
+
+class Actions extends Component {
+  static propTypes = {
+    playerTriesToHit: PropTypes.func.isRequired,
+  };
+
+  playerClicksFight = () => {
+    this.props.playerTriesToHit();
+  }
+  
+  render() {
+    return (
+      <section className={styles.userActions}>
+        <ul>
+          <li>Fight</li>
+          <li>Heal</li>
+          <li>Special</li>
+          <button type="Fight" onClick={this.playerClicksFight}>Fight</button>
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default Actions;
