f3f94b9becb2c193b9924b151aac08a85d62889e
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-01 22:27:19 -0800

    brought main container comp into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import LayoutComp from '../components/layout/LayoutComp';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <LayoutComp />
+    </>
+  );
 }
-  
