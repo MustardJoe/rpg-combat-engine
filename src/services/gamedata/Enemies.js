fdf9dc6a83e1788346cd6c6e0b347cb9c12299e1
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-01 21:32:07 -0800

    basic enemies data

---
+++ src/services/gamedata/Enemies.js
@@ -0,0 +1,32 @@
+const enemies = [
+  {
+    name: 'Goomba',
+    damage: 1,
+    strengthBonus: 0,
+    armorClass: 1,
+    armor: 0,
+    dexterity: 5,
+    hitPoints: 2,
+  },
+  {
+    name: 'Goblin',
+    damage: 2,
+    strengthBonus: 0,
+    armorClass: 5,
+    armor: 0,
+    dexterity: 5,
+    hitPoints: 6,
+  },
+  {
+    name: 'Ninja Master',
+    damage: 25,
+    strengthBonus: 5,
+    armorClass: 18,
+    armor: 5,
+    dexterity: 20,
+    hitPoints: 50,
+  }
+
+];
+
+export default enemies;
