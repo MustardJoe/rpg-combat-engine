eae3543ad2f3f95e05d91ed689c1bcbaf491d91c
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-02 16:51:09 -0800

    fleshed out player data a bit more

--- src/services/gamedata/Players.js
+++ src/services/gamedata/Players.js
@@ -1,9 +1,13 @@
 const Players = [
   {
     name: 'Riptor the Butt Damager',
-    hitBonus: 1,
+    HP: 20,
     armorClass: 12,
+    hitBonus: 1,
+    agility: 15,
+    strength: 15,
     healPotions: 3,
+    status: 'Normal',
   }
 ];
 
