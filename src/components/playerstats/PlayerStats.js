02c83c0b42ae685a44f7e0c932d79349a30344f2
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-02 16:50:04 -0800

    made player stats dynamic and style tweaks

--- src/components/playerstats/PlayerStats.js
+++ src/components/playerstats/PlayerStats.js
@@ -2,26 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './playerstats.css';
 
-function YourStats() {
-  const dummyStats = {
-    HP: 80,
-    Armor: 12,
-    Agility: 15,
-    Strength: 15,
-    Status: 'Normal',
-  };
+function PlayerStats({ playerStatsObj }) {
+  const {
+    name,
+    HP,
+    armorClass,
+    agility,
+    strength,
+    healPotions,
+    status,
+  } = playerStatsObj;
 
   return (
     <div className={styles.yourStatsWindow}>
+      <span>{name}</span>
       <ul className={styles.yourStatsList}>
-        <li>HP: {dummyStats.HP}</li>
-        <li>Armor: {dummyStats.Armor}</li>
-        <li>Agility: {dummyStats.Agility}</li>
-        <li>Strength: {dummyStats.Strength}</li>
-        <li>Status: {dummyStats.Status}</li>
+        <li>Hit Points: {HP}</li>
+        <li>Armor Class: {armorClass}</li>
+        <li>Agility: {agility}</li>
+        <li>Strength: {strength}</li>
+        <li>Status: {status}</li>
+        <li>Potions of Healing: {healPotions}</li>
       </ul>
     </div>
   );
 }
 
-export default YourStats;
+PlayerStats.propTypes = {
+  playerStatsObj: PropTypes.shape({
+    name: PropTypes.string,
+    HP: PropTypes.number,
+    armorClass: PropTypes.number,
+    agility: PropTypes.number,
+    strength: PropTypes.number,
+    healPotions: PropTypes.number,
+    status: PropTypes.number,
+  })
+};
+
+export default PlayerStats;
