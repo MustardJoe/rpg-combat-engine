6628e5ad66c0ce8b1972c7bddfba2df4b7b31315
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-02 19:49:07 -0800

    made fight a universal action works for player of enemy now

--- src/services/combatengine/CombatEngine.js
+++ src/services/combatengine/CombatEngine.js
@@ -1,38 +1,55 @@
 const CombatEngine = {
 
-  player: {
-    fight: function(enemyAC, playerHitBonus) {
+  enemyActions: {
+    actionsList: ['fight'],
+  },
+
+  combatMsgs: {
+    player: ['You hit the enemy', 'You missed the enemy'],
+    enemy: ['You are hit by the enemy', 'The enemy misses you'],
+  },
+
+  universalActions: {
+    fight: function(armorClass, hitBonus, hitPoints, damageD, currentTurn) {
       let fightActionReturnObject = {};
-      console.log('enemy AC', enemyAC);
-      console.log('player hit bonus', playerHitBonus);
-      let thisRoll = CombatEngine.dieRolls.d20() + playerHitBonus;
+      /* eslint-disable-next-line no-console */
+      console.log('in CombatEngine.js - AC', armorClass);
+      /* eslint-disable-next-line no-console */
+      console.log('hit bonus', hitBonus);
+      let thisRoll = CombatEngine.dieRolls.d20() + hitBonus;
+      /* eslint-disable-next-line no-console */
       console.log('you rolled a ' + thisRoll);
-      if(thisRoll >= enemyAC) {
-        fightActionReturnObject.combatMsg = 'you hit the enemy';
-        fightActionReturnObject.damage = CombatEngine.dieRolls.d8();
+      if(thisRoll >= armorClass) {
+        fightActionReturnObject.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][0];
+        fightActionReturnObject.damage = CombatEngine.dieRolls[`d${damageD}`]();
+        fightActionReturnObject.newHP = hitPoints - fightActionReturnObject.damage;
+        if(fightActionReturnObject.newHP < 0) fightActionReturnObject.newHP = 0;
       } 
       else {
-        fightActionReturnObject.combatMsg = 'you missed the enemy';
+        fightActionReturnObject.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][1];
         fightActionReturnObject.damage = 0; 
+        fightActionReturnObject.newHP = hitPoints;
       }
       return fightActionReturnObject;
     },
   },
 
-  enemy: {
-    fight: function(playerAC, enemyHitBonus) {
-      let fightActionReturnObject = {};
-      return fightActionReturnObject;
-    },
-  },
-
   dieRolls: {
+    d25: function() {
+      return Math.floor((Math.random()) * 25 + 1);
+    },
     d20: function() {
       return Math.floor((Math.random()) * 20 + 1);
     },
     d8: function() {
       return Math.floor((Math.random()) * 8 + 1);
-    } 
+    },
+    d2: function() {
+      return Math.floor((Math.random()) * 2 + 1);
+    },
+    d1: function() {
+      return 1;
+    },
   }
 };
 
