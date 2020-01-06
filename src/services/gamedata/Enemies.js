f1fcc0f1f866fc202586520dae653d40cbfd6f29
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-06 15:44:52 -0800

    added temp images

--- src/services/gamedata/Enemies.js
+++ src/services/gamedata/Enemies.js
@@ -6,6 +6,7 @@ const enemies = [
     hitBonus: 0,
     damage: 1,
     dexterity: 5,
+    img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg4.wikia.nocookie.net%2F__cb20120916174616%2Fmario-enemies%2Fimages%2F9%2F93%2FGoomba2-1-.jpg&f=1&nofb=1',
   },
   {
     name: 'Goblin',
@@ -14,6 +15,7 @@ const enemies = [
     hitBonus: 0,
     damage: 2,
     dexterity: 5,
+    img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffantasy-faction.com%2Fwp-content%2Fuploads%2F2014%2F10%2FGoblin.jpg&f=1&nofb=1'
   },
   {
     name: 'Ninja Master',
@@ -22,6 +24,7 @@ const enemies = [
     hitBonus: 5,
     damage: 25,
     dexterity: 20,
+    img: '',
   }
 
 ];
