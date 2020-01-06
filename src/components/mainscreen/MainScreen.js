48c7680983e155994a2be1a277f8eb4a59973da1
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-06 15:45:22 -0800

    added styling to main screen comp

--- src/components/mainscreen/MainScreen.js
+++ src/components/mainscreen/MainScreen.js
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './mainscreen.css';
 
-function MainScreen({ currentCombatMsg }) {
+function MainScreen({ currentCombatMsg, enemyImg }) {
   return (
     <section className={styles.mainScreen}>
       <p>{currentCombatMsg}</p>
+      <p><img className={styles.img} src={enemyImg} alt="image of a badie"></img></p>
     </section>
   );
 }
 
 MainScreen.propTypes = {
   currentCombatMsg: PropTypes.string,
+  enemyImg: PropTypes.string,
 };
 
 export default MainScreen;
