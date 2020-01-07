d5757577fb688968f993fef137536fde8aae9f34
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-06 17:09:24 -0800

    have rough heal action

--- src/services/combatengine/CombatEngine.js
+++ src/services/combatengine/CombatEngine.js
@@ -8,10 +8,16 @@ const CombatEngine = {
     player: ['You hit the enemy', 'You missed the enemy'],
     enemy: ['You are hit by the enemy', 'The enemy misses you'],
   },
+  healMsgs: {
+    player: ['You guzzle a healing potion',
+      'You try to drink a potion of healing, but none remain'],
+    enemy: ['the enemy drinks a healing potion',
+      'The enemy fails to drink a healing potion'],
+  },
 
   universalActions: {
     fight: function(armorClass, hitBonus, hitPoints, damageD, currentTurn) {
-      let fightActionReturnObject = {};
+      let fightActionReturnObj = {};
       /* eslint-disable-next-line no-console */
       console.log('in CombatEngine.js - AC', armorClass);
       /* eslint-disable-next-line no-console */
@@ -20,18 +26,38 @@ const CombatEngine = {
       /* eslint-disable-next-line no-console */
       console.log('you rolled a ' + thisRoll);
       if(thisRoll >= armorClass) {
-        fightActionReturnObject.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][0];
-        fightActionReturnObject.damage = CombatEngine.dieRolls[`d${damageD}`]();
-        fightActionReturnObject.newHP = hitPoints - fightActionReturnObject.damage;
-        if(fightActionReturnObject.newHP < 0) fightActionReturnObject.newHP = 0;
+        fightActionReturnObj.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][0];
+        fightActionReturnObj.damage = CombatEngine.dieRolls[`d${damageD}`]();
+        fightActionReturnObj.newHP = hitPoints - fightActionReturnObj.damage;
+        if(fightActionReturnObj.newHP < 0) fightActionReturnObj.newHP = 0;
       } 
       else {
-        fightActionReturnObject.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][1];
-        fightActionReturnObject.damage = 0; 
-        fightActionReturnObject.newHP = hitPoints;
+        fightActionReturnObj.combatMsg = CombatEngine.combatMsgs[`${currentTurn}`][1];
+        fightActionReturnObj.damage = 0; 
+        fightActionReturnObj.newHP = hitPoints;
       }
-      return fightActionReturnObject;
+      return fightActionReturnObj;
     },
+
+    heal: function(hitPoints, healPotions, maxHP, currentTurn) {
+      let healActionReturnObj = {};
+      let healAmount = CombatEngine.dieRolls.universal(5) + 5;
+      if(healPotions > 0) {
+        healActionReturnObj.actionMsg = CombatEngine.healMsgs[`${currentTurn}`][0];
+        healActionReturnObj.newHP = hitPoints + healAmount;
+        if(healActionReturnObj.newHP > maxHP) healActionReturnObj.newHP = maxHP;
+        healActionReturnObj.remainingHealPotions = healPotions - 1;
+      }
+      if(healPotions <= 0) {
+        healActionReturnObj.actionMsg = CombatEngine.healMsgs[`${currentTurn}`][1];
+        healActionReturnObj.newHP = hitPoints;
+        healActionReturnObj.remainingHealPotions = 0;
+      }
+      console.log('heal amount', healAmount);
+      console.log('returnObj', healActionReturnObj);
+      return healActionReturnObj;
+
+    }
   },
 
   dieRolls: {
@@ -44,12 +70,16 @@ const CombatEngine = {
     d8: function() {
       return Math.floor((Math.random()) * 8 + 1);
     },
+
     d2: function() {
       return Math.floor((Math.random()) * 2 + 1);
     },
     d1: function() {
       return 1;
     },
+    universal: function(sides) {
+      return Math.floor((Math.random()) * sides + 1);
+    }
   }
 };
 
