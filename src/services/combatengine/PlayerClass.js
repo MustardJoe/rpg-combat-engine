743d34c5252a248047da3c5f38efac9f08c63660
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-13 19:57:11 -0800

    renamed new file to player class

--- src/services/combatengine/PlayerClass.js
+++ src/services/combatengine/PlayerClass.js
@@ -5,7 +5,7 @@ class Player {
     maxHP, 
     armorClass, 
     hitBonus, 
-    damage, 
+    damageD, 
     agility, 
     strength, 
     healPotions, 
@@ -13,12 +13,12 @@ class Player {
     alive,
   }) {
     this.state = {
-      name: name || 'Riptor the Butt Damager',
+      name: name || 'Riptor the Fighty',
       hitPoints: hitPoints || 20,
       maxHP: maxHP || 20,
       armorClass: armorClass || 12,
       hitBonus: hitBonus || 1,
-      damage: damage || 8,
+      damageD: damageD || 8,
       agility: agility || 15,
       strength: strength || 15,
       healPotions: healPotions || 3,
@@ -27,7 +27,7 @@ class Player {
     };
   }
   attack = (enemy, currentTurn) => {
-    if(!currentTurn) {
+    if(currentTurn != 'player') {
       return null;
     }
     const rollValue = this._getRoll(20, this.state.hitBonus);
@@ -36,16 +36,21 @@ class Player {
     }
   }
 
-  heal = () => {}
+  heal = () => {
+    if(this.state.healPotions > 0) {
+      this.state.healPotions = this.state.healPotions - 1;
+      this.state.hitPoints += this._getRoll(5, 5);
+    }
+  }
 
-  getStatus = (status) => this.state[status]
+  getStatus = (status) => this.state[status];
 
   _getRoll = (sides, bonus = 0) => {
     return Math.floor((Math.random()) * sides + 1) + bonus;
   }
 
   _damageEnemy = (enemy) => {
-    let damage = this._getRoll(this.state.damage);
+    let damage = this._getRoll(this.state.damageD);
     enemy.state.hitPoints = enemy.state.hitPoints - damage;
     if(enemy.state.hitPoints < 0) {
       enemy.state.hitPoints = 0;
