a6c9418f64045f0e1332ba21f8dceb7f09fd2b96
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-13 19:58:54 -0800

    temp change to try lionel's thing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import LayoutComp from '../components/layout/LayoutComp';
+import EngineWrapper from '../services/combatengine/EngineWrapper';
 
 export default function App() {
   return (
     <>
       <h1>GriNd-O-TRoN</h1>
       <h3>A turn-based combat engine</h3>
-      <LayoutComp />
+      <EngineWrapper />
     </>
   );
 }
