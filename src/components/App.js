6f72bc62620a0c3bb20efa4f8d8a1d99df4eec7f
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-26 18:09:36 -0800

    basic app styling of header area

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LayoutComp from '../components/layout/LayoutComp';
 import EngineWrapper from '../services/combatengine/EngineWrapper';
+import grindotronlogo from '../assets/grindotronlogo.png';
+import styles from './app.css';
 
 export default function App() {
   return (
     <>
-      <h1>GriNd-O-TRoN</h1>
-      <h3>A turn-based combat engine</h3>
+      <p className={styles.logop}><img src={grindotronlogo} alt="GriNd-O-TRoN" className={styles.logo}></img></p>
+      <h3 className={styles.sublogo}>A turn-based combat engine</h3>
       <LayoutComp />
     </>
   );
