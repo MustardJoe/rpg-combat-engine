24b172e910dceccf8af7eea2e4e261ec08f59d39
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-27 23:31:40 -0800

    added game over comp and img asset

---
+++ src/components/gameover/GameOver.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import grindogameover from '../../assets/grindogameover.png';
+
+function GameOver() {
+  return (
+    <div>
+      <img src={grindogameover} alt='Game Over'></img>
+    </div>
+  );
+
+}
+
+export default GameOver;
