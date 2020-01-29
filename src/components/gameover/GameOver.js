bea223da009fa76a07211bc86c5258e70065ab47
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-28 22:52:07 -0800

    styled gameOver comp

--- src/components/gameover/GameOver.js
+++ src/components/gameover/GameOver.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import grindogameover from '../../assets/grindogameover.png';
+import styles from './gameover.css';
 
 function GameOver() {
   return (
-    <div>
+    <div className={styles.gameOver}>
       <img src={grindogameover} alt='Game Over'></img>
     </div>
   );
