18888eb07fc6fe56148eeb4fea596c143e5548f3
Jonathan Forney <jonathan.forney@gmail.com>
2020-01-31 18:13:21 -0800

    got rand enemy death msg working

--- src/services/gamedata/Msgs.js
+++ src/services/gamedata/Msgs.js
@@ -14,21 +14,29 @@ const Msgs = {
       'You have no more hit points and you have died.',
       'Oh no… Sadly, you are not victorious in your battle.',
       'You hear an evil laugh: "MUAHAHAHHAHAHAHAHAA...." You are dead.',
-      'Here\'s hoping the next adventurer has more luck than you...',
+      'Here\'s hoping the next adventurer has more luck...',
       'You get dysentery, your oxen have died, and your wagon broke an axel...',
-      'Thanks for playing, but you died.'],
-    enemy: `You have bested your foe! Your enemy falls to
+      'Thanks for playing, but you died.',
+      'Your sould was consumed by the evil!'],
+    enemy: [
+      `You have bested your foe! Your enemy falls to
             the ground and explodes in a cloud of glitter!`,
+      'Not too bad, I guess...',
+      'You whacked that beastie darn good!'
+    ],
+    randPlayerDieMsg: function() {
+      let randNumb = Math.floor(Math.random() * (Msgs.deathMsgs.player.length));
+      let Msg = Msgs.deathMsgs.player[randNumb];
+      return Msg;  
+    },
+  
+    randEnemyDieMsg: function() {
+      let randNumb = Math.floor(Math.random() * (Msgs.deathMsgs.enemy.length));
+      let Msg = Msgs.deathMsgs.enemy[randNumb];
+      return Msg;  
+    }
   },
   runMsg: 'Like a coward, you turn your tail and run from the beast.',
-
-  randPlayerDieMsg: function() {
-    let randNumb = Math.floor(Math.random() * (Msgs.deathMsgs.player.length));
-    let Msg = Msgs.deathMsgs.player[randNumb];
-    return Msg;  
-  }
-  
-
 };
 
 export default Msgs;
