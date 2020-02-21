ba44a3626c3703c22ac6a710b7d04c7b6871ce0e
Jonathan Forney <jonathan.forney@gmail.com>
2020-02-20 20:53:18 -0800

    actions buttons styling

--- src/components/actions/Actions.js
+++ src/components/actions/Actions.js
@@ -33,28 +33,34 @@ class Actions extends Component {
   render() {
     return (
       <section className={styles.userActions}>
-        Player Actions
+        Player Actions:
         <ul className={styles.actionsList}>
           <li>
-            <button type="Fight" 
+            <button type="Fight"
+              className={styles.buttons}
               style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}}
               id="fightButton" onClick={this.playerClicksFight}
               disabled={this.props.playerMadeChoice}>Fight</button>
           </li>
           <li>
-            <button type="Heal" 
+            <button type="Heal"
+              className={styles.buttons}
               style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}} 
               id="healButton" onClick={this.playerClicksHeal}
               disabled={this.props.playerMadeChoice}>Heal</button>
           </li>
           <li>
-            <button type="Special" style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}} 
+            <button type="Special" 
+              className={styles.buttons}
+              style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}} 
               id="specialButton" onClick={this.playerClicksSpecial}
               disabled={this.props.playerMadeChoice}>Special</button>
           </li>
           <li>Defend</li>
           <li>
-            <button type="Run" style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}} 
+            <button type="Run" 
+              className={styles.buttons}
+              style={this.props.playerMadeChoice ? { backgroundColor : 'yellow' } : {}} 
               id="runButton" onClick={this.playerClicksRun}
               disabled={this.props.playerMadeChoice}>Run</button>
           </li>
