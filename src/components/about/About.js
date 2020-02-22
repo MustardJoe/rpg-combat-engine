96481dd7919079e233ff239cd1284ce631a9d5d8
Jonathan Forney <jonathan.forney@gmail.com>
2020-02-21 23:22:18 -0800

    more about comp refining

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './about.css';
+import selfie from '../../assets/jonselfie.png';
 
 export default function About() {
   return (
@@ -18,7 +19,23 @@ export default function About() {
         are awarded, and when accumulated in sufficient quantity, the player
         character gains a level and becomes more powerful. This cycle,
         fighting battles over and over again to become more powerful, is
-        known as grinding, and is where the app name comes from.
+        known as grinding, and is where the name comes from.
+      </p>
+      <p className={styles.paragraph}>
+        <img src={selfie}
+          className={styles.selfie}
+          alt="a pixelized rending  of the app's author"></img>
+        <p className={styles.divInPara}>
+
+          If you enjoy GriNd-O-TRoN, please consider checking out more of my
+          projects here on GitHub, or on <a className={styles.link}
+            href="http://www.jonforney.com/">www.jonforney.com</a>.
+          Additionally,
+          I'm currently looking for work, so please feel free to visit <a
+            className={styles.link}
+            href="https://www.linkedin.com/in/jon-forney/">my linked in page</a> or
+          contact me directly with your professional opportunies.
+        </p>
       </p>
     </section>
 
